5bf99d009dacdb506ded50e5ae55b003154567c4
Amit Upadhyay <amitu@cdac.in>
2025-06-02 11:38:29 +0530

    commit

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+        ]
+      }
+    ]
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: 'http://to-do-app-env.eba-pj9gtcax.us-east-1.elasticbeanstalk.com'
+  }
+};
+
+module.exports = nextConfig; 
